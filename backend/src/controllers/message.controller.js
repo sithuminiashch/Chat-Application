eddaf058f14729f0be8f1d43145c09b5acb8478d
sithuminiashch <136596859+sithuminiashch@users.noreply.github.com>
2025-11-12 19:04:47 +0530

    Backend Intialization

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,85 @@
+import { getReceiverSocketId, io } from "../lib/socket.js";
+import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
+
+
+export const getUserForSideBar=async(req,res)=>{
+    try{
+
+        const loggedInUserId=req.user._id;
+        const filteresdUsers=await User.find({_id:{$ne:loggedInUserId}}).select("-password");
+
+        res.status(200).json(filteresdUsers);
+
+    }
+    catch(error)
+    {
+        console.log("Error in getUsersForSideBar: ",error.message);
+        res.status(500).json({error:"Internal Server Error"})
+    }
+
+};
+
+export const getMessages=async(req,res)=>
+{
+    try{
+        const{id:userToChatId}=req.params;
+        const myId=req.user._id;
+
+        const messages=await Message.find({
+            $or:[
+                {senderId:myId,receiverId:userToChatId},
+                {senderId:userToChatId,receiverId:myId},
+            ],
+        });
+        res.status(200).json(messages);
+
+    }catch(error)
+    {
+
+        console.log("Error in Message Controller",error.message);
+        res.status(500).json({error:"Internal server Error"});
+
+    }
+}
+
+export const sendMessage=async(req,res)=>{
+    try{
+        const{text,image}=req.body;
+        const{id:receiverId}=req.params;
+        const senderId=req.user._id;
+
+        let imageUrl;
+        if(image){
+            const uploadResponse=await cloudinary.uploader.upload(image);
+            imageUrl=uploadResponse.secure_url;
+        }
+
+        const newMessage=new Message({
+            senderId,
+            receiverId,
+            text,
+            image:imageUrl,
+
+        });
+
+        await newMessage.save();
+
+        const receiverSocketId=getReceiverSocketId(receiverId);
+        if(receiverSocketId)
+        {
+            io.to(receiverSocketId).emit("newMessage",newMessage);
+        }
+
+        res.status(201).json(newMessage)
+
+    }
+    catch(error)
+    {
+        console.log("Error in Sendmessage Controller",error.message);
+        res.status(500).json({error:"Internal Server Error"});
+
+    }
+}
+
+
---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import {connectDB} from "./lib/db.js"
+import { app, server } from "./lib/socket.js";
+
+dotenv.config()
+
+app.use(express.json());
+app.use(cookieParser());
+const PORT=process.env.PORT;
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
+app.use(express.json({ limit: "1000000mb" })); // increase to 10mb
+app.use(express.urlencoded({ limit: "1000000mb", extended: true }));
+
+app.use("/api/auth",authRoutes);
+app.use("/api/messages",messageRoutes);
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
